de7a628fc0b3de8973e55349204f3690a6e2086b
madchacky <44056222+chackydude@users.noreply.github.com>
2019-09-10 16:28:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,17 +47,70 @@ class Game {
 	}
 
 	createPiece() {
-	 
+	 	const index = Math.floor(Math.random() * 7);
+	 	const type ='IJLOSTZ'[index];
+	 	const piece = {};
 
-		return {
-		x: 0,
-		y: 0,
-		blocks: [
-		 [0, 1, 0],	
-		 [1, 1, 1],	
-		 [0, 0, 0]
-		]
-		}; 
+	 	switch (type) {
+	 		case 'I':
+	 			piece.blocks = [
+	 				[0,0,0,0],
+	 				[1,1,1,1],
+	 				[0,0,0,0],
+	 				[0,0,0,0]
+	 			];
+	 			break;
+	 		case 'J':
+	 			piece.blocks = [
+	 				[0,0,0],
+	 				[2,2,2],
+	 				[0,0,2]
+	 			];
+	 			break;
+	 		case 'L':
+	 			piece.blocks = [
+	 				[0,0,0],
+	 				[3,3,3],
+	 				[3,0,0]
+	 			];
+	 			break;
+	 		case 'O':
+	 			piece.blocks = [
+	 				[0,0,0,0],
+	 				[0,4,4,0],
+	 				[0,4,4,0],
+	 				[0,0,0,0]
+	 			];
+	 			break;
+	 		case 'S':
+	 			piece.blocks = [
+	 				[0,0,0],
+	 				[0,5,5],
+	 				[5,5,0]
+	 			];
+	 			break;
+	 		case 'T':
+	 			piece.blocks = [
+	 				[0,0,0],
+	 				[6,6,6],
+	 				[0,6,0]
+	 			];
+	 			break;
+	 		case 'Z':
+	 			piece.blocks = [
+	 				[0,0,0],
+	 				[7,7,0],
+	 				[0,7,7]
+	 			];
+	 			break;
+	 		default: 
+	 			throw new Error('Неизвестный тип фигуры');
+	 	}
+
+	 	piece.x = Math.floor((10 - piece.blocks[0].length)/2);
+	 	piece.y = -1;
+
+		return piece;
 	}
 
 	movePieceLeft() {
@@ -106,7 +159,7 @@ class Game {
 
 				if (clockwise) {
 					blocks[i][j] = blocks[y - j][i];
-					blocks[y - j][i] = blocks[y - i][y - j];
+		     			blocks[y - j][i] = blocks[y - i][y - j];
 					blocks[y - i][y - j] = blocks[j][y - i];
 					blocks[j][y - i] = temp;
 				} else {
@@ -158,6 +211,16 @@ class Game {
 };
 
 class View {
+	static colors = {
+		'1': 'cyan',
+		'2': 'blue',
+		'3': 'orange',
+		'4': 'yellow',
+		'5': 'green',
+		'6': 'purple',
+		'7': 'red'
+	};
+
 	constructor(element, width, height, rows, colums) {
 		this.element = element;
 		this.width = width; 
@@ -191,7 +254,7 @@ class View {
 					const block = line[x];
 
 					if (block) {
-						this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, 'red');
+						this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, View.colors[block]);
 				}
 			}
 		}
